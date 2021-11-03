c1416cb2f47d7fc9c477ea6786f56242214590e1
Keisha Harris <keishadharriss@gmail.com>
2021-11-03 14:33:55 -0400

    push

--- src/components/MessageArea.js
+++ src/components/MessageArea.js
@@ -2,31 +2,13 @@ import React from "react";
 import "./MessageArea.css"
 
 export default function MessageArea(headline, sender, receiver) {
-
+    
     return (
         <div className="message-area">
             <div id="headline">
                 Chat Conversation with receiver.
             </div>
             <div className="messages">
-                <div id="receiver">
-                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                </div>
-                <div id="sender">
-                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                </div>
-                <div id="receiver">
-                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                </div>
-                <div id="sender">
-                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                </div>
-                <div id="receiver">
-                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                </div>
-                <div id="sender">
-                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                </div>
             </div>
         </div>
     )
--- src/components/SendBox.js
+++ src/components/SendBox.js
@@ -6,57 +6,92 @@ import { RiDeleteBin5Line } from 'react-icons/ri'
 import { FaStop } from 'react-icons/fa'
 
 
+const appendElement = (classNm, content) => {
+	let messages = document.getElementById('messages');
+	const node = document.createElement("DIV");
+	if (classNm == "sender") {
+		let audio = document.createElement('audio');
+		audio.controls = true;
+		audio.autoplay = true;
+		node.appendChild(audio);
+		messages.appendChild(node)
+	}
+
+	
+}
 
 export default function SendBox(){
-	const [audio, setRecord] = useState('inactive');
+
+	let MediaRecorder;
+
 	const [audioChunks, setAudioChunks] = useState([]);
 	// const [audioblob, setAudioBlob] = useState('');
-	let rec;
-	//   const getStream = () => {
+	// let rec;
+	  const getStream = () => {
 		if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
 			alert('Your browser does not support recording!');
-		  }
+		}
 	
 		  navigator.mediaDevices
 		 .getUserMedia({ audio: true })
 		.then((stream) => {
-			console.log('fan ', stream);
-			getRecord(stream);
+			console.log('stream ', stream);
+			MediaRecorder = new MediaRecorder(stream);
 		 })
 	  .catch((err) => console.log("The following gUM error occured: " + err));
-	//   }
+	  }
 	
 
-	//   getStream();
+	  getStream();
 
-	  function getRecord(stream) {
-		  console.log('as ', stream)
-		rec = new MediaRecorder(stream);
-		console.log('as1 ', rec.ondataavailable)
-		rec.ondataavailable = (e) => {
-			console.log("ondataavailable", e.data);
-			setAudioChunks(audioChunks.push(e.data));
-		  console.log("speech",e.data);
-		  if (rec.state == "inactive") {
-			const audioBlob = new Blob(audioChunks, { type: "audio/ogg; codecs=opus" });
-			console.log('test blob', audioBlob);
-			console.log('audioblob ', audioBlob);
-		//    appendEl('recordedAudio', audioBlob);
-		  }
+	//   function getRecord() {
+	// 	MediaRecorder.ondataavailable = (e) => {
+	// 		console.log("ondataavailable", e.data);
+	// 		setAudioChunks(audioChunks.push(e.data));
+			
+	// 	  if (MediaRecorder.state == "inactive") {
+			  
+	// 		const audioBlob = new Blob(audioChunks, { type: "audio/ogg; codecs=opus" });
+	// 		console.log('audioblob ', audioBlob);
+	// 		appendElement('sender', audioBlob);
+	// 	//   appendEl('recordedAudio', audioBlob);
+	// 	  }
+	// 	};
+	//   }
+
+	async function sendData(audioBlob) {
+		let base64data;
+		const reader = new window.FileReader();
+		await reader.readAsDataURL(audioBlob);
+		reader.onloadend = await function() {
+			base64data=reader.result;
+
+			console.log("base", base64data)
 		};
-	  }
+	}
 
 	  const handleRecord = (e) => {
-		  console.log('start', e)
-		//   if(audio == 'inactive'){
-		// 	console.log('record start', e);
-		// 	rec.start();
-		// 	setRecord('active');
-		// 	console.log('rec state', rec.state);
-		//   }
+		  console.log('event', e)
+		  if(e === 'active'){
+			//  console.log('start', MediaRecorder)
+			MediaRecorder.start();
+		  }
 		//   else{
-		//   	setRecord('inactive');
-		//   	// getRecord();
+		// 	// console.log('rec state ', MediaRecorder.state)
+		// 	MediaRecorder.stop();
+		// 	MediaRecorder.ondataavailable = (e) => {
+		// 		console.log("ondataavailable", e.data);
+		// 		setAudioChunks(audioChunks.push(e.data));
+				
+		// 	  if (MediaRecorder.state == "inactive") {
+				  
+		// 		const audioBlob = new Blob(audioChunks, { type: "audio/ogg; codecs=opus" });
+		// 		console.log('audioblob ', audioBlob);
+		// 		appendElement('sender', audioBlob);
+		//		sendData(audioBlob)
+		// 	//   appendEl('recordedAudio', audioBlob);
+		// 	  }
+		// 	};
 		//   }
 	  }
 
@@ -66,15 +101,15 @@ export default function SendBox(){
 			<RiDeleteBin5Line size="1em" color="gray"/>
 
 			{/*Record/Send Button */}
-			<div id="recordStart" className="recordButton" onClick={handleRecord}>
+			<div id="recordStart" className="recordButton" onClick={(e) => handleRecord("active")}>
 			
 			<GiOldMicrophone size="1.5em" color="white"/> 
 			</div>
 			
-			<div id="recordstop" className="recordButton" onClick={(e) => handleRecord}>
+			<div id="recordStop" className="recordButton" onClick={(e) => handleRecord("inactive")}>
 			<FaStop color="white"/>
 			</div>
-			<BsFillPlayFill size="1em" color="gray"/>
+			<BsFillPlayFill size="1em" color="gray"/> 
 		</div>   
         )
 }
